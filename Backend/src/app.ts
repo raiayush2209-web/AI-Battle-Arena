99d790b7cbcb0c0b71df50c088ef7e9936667654
Ayush rai <raiayush2209-web>
2026-09-06 22:17:45 +0530

    Allow production frontend

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -13,7 +13,8 @@ app.use(cors({
    origin: [
     "http://localhost:5173",
     "http://localhost:5175",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://ai-battle-arena.vercel.app"
 
   ]
   ,  methods: ["GET", "POST", "PATCH", "DELETE"],
